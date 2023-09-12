7df6d5753b1f79e3c6c707bc7b1e369e525b0eaf
Steven <stevenxchung@gmail.com>
2023-09-11 21:11:34 -0400

    Add initial e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { AppService } from '../src/app.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('App (e2e)', () => {
+  let appService: AppService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    appService = moduleFixture.get<AppService>(AppService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should run the service on initialization', async () => {
+    const loggerSpy = jest.spyOn(Logger.prototype, 'log');
+
+    await appService.onModuleInit();
+
+    expect(loggerSpy).toHaveBeenCalledWith('Initiate indexing...');
+    expect(loggerSpy).toHaveBeenCalledWith(
+      'Indexing complete! Printing highest token-value accounts by account type...',
+    );
   });
 });
